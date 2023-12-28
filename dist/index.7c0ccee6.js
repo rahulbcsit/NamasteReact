288539605f49f524a899747dd877af7a0c7e4998
Rahul Gupta-Gunners <rahulbcsit@hotmail.com>
2023-12-28 12:05:06 -0600

    add the gitignore

---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,20 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "i am h1 tag"),
+        React.createElement("h2", {}, "i am h2 tag")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h3", {}, "i am h3 tag"),
+        React.createElement("h4", {}, "i am h4 tag")
+    ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
